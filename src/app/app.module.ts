2944b73a3d920813929987c9c1420fdf3b89e96c
claudiodaniel.carrizo93 <claudiodaniel.carrizo93@gmail.com>
2021-09-24 12:34:26 -0300

    add navbar and carrousel with info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Components
 import { AppComponent } from './app.component';
 import { PresentationComponent } from './component/home/presentation/presentation.component';
 import { ActivitiesComponent } from './component/home/activities/activities.component';
+import { MenuComponent } from './component/base/menu/menu.component';
+import { FooterComponent } from './component/base/footer/footer.component';
+
+//AngularMaterial
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PresentationComponent,
-    ActivitiesComponent
+    ActivitiesComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
