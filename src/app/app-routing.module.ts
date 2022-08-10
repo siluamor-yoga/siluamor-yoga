c42cc2051684dce27774bdf2ca28ac3949221ea0
claudio.carrizo <claudio.carrizo@wenance.com>
2022-08-10 12:30:45 -0300

    update angular version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SidenavComponent } from './component/base/sidenav/sidenav.component';
-import { ActivitiesComponent } from './component/home/activities/activities.component';
-import { PresentationComponent } from './component/home/presentation/presentation.component';
-import { ProfileComponent } from './component/home/profile/profile.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/presentation', pathMatch: 'full' },
-  { path: 'presentation', component: PresentationComponent },
-  { path: 'profile', component: ProfileComponent },
-  //{ path: 'sidenav', component: SidenavComponent },
-  { path: 'activities', component: ActivitiesComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-//Components
 import { AppComponent } from './app.component';
-import { PresentationComponent } from './component/home/presentation/presentation.component';
-import { ActivitiesComponent } from './component/home/activities/activities.component';
-import { MenuComponent } from './component/base/menu/menu.component';
-import { FooterComponent } from './component/base/footer/footer.component';
-import { ProfileComponent } from './component/home/profile/profile.component';
-import { ResumeComponent } from './component/home/presentation/resume/resume.component';
-import { DetailComponent } from './component/home/presentation/detail/detail.component';
-import { SidenavComponent } from './component/base/sidenav/sidenav.component';
-
-//AngularMaterial
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { SidenavListComponent } from './component/base/sidenav/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PresentationComponent,
-    ActivitiesComponent,
-    MenuComponent,
-    FooterComponent,
-    ProfileComponent,
-    ResumeComponent,
-    DetailComponent,
-    SidenavComponent,
-    SidenavListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatListModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatToolbarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
